49fe67a1b2e39d5f6ff60b7c3a5f3d8b191aefb0
Cristian Yepes <ycristian@uninorte.edu.co>
2021-03-01 10:27:52 -0500

    validate description

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :social_networks, dependent: :destroy
   has_one_attached :avatar
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 6, maximun: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximun: 300 }
 end
