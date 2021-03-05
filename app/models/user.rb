960127ee77be04f45308299b0882262ceb03c2b9
Cristian Yepes <ycristian@uninorte.edu.co>
2021-03-05 12:31:01 -0500

    fixed registration update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   #accepts_nested_attributes_for :social_networks, reject_if: :all_blank, allow_destroy: true
   has_one_attached :avatar
 
-  validates :name, presence: true, length: { minimum: 6, maximun: 100 }
+  validates :name, presence: true, length: { minimum: 6, maximun: 50 }
   validates :username, presence: true, length: { minimum: 6, maximun: 20 }
-  #validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates :username, :format => { :with => /\A(?=.*[a-z])[a-z\d]+\Z/i }
   #validates :description, length: { minimum: 10, maximun: 300 }
 end
